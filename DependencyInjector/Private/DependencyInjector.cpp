4762793bd79d3c4a96858043cae7c1b7e9dd1168
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2024-01-09 01:12:49 +0000

    Create DependencyInjector.cpp

---
+++ DependencyInjector/Private/DependencyInjector.cpp
@@ -0,0 +1,63 @@
+// ©2023 JDSherbert. All Rights Reserved.
+
+#include "DependencyInjector.h"
+
+/* ------------------------------ Initializations --------------------------------- */
+
+UDependencyInjector* UDependencyInjector::Instance = nullptr;
+
+/* ---------------------------- Method Definitions ------------------------------- */
+
+void UDependencyInjector::RegisterObject(UObject* InjectionObject)
+{
+    if (!Instance)
+    {
+        // Create the singleton instance if it doesn't exist
+        Instance = NewObject<UDependencyInjector>();
+    }
+
+    const UClass* ObjectClass = InjectionObject->GetClass();
+    Instance->RegisteredObjects.FindOrAdd(ObjectClass).Add(InjectionObject);
+}
+
+/* ------------------------------------------------------------------------------- */
+
+void UDependencyInjector::UnregisterObject(UObject* InjectionObject)
+{
+    if (!Instance)
+    {
+        // The instance doesn't exist, so nothing to unregister
+        return;
+    }
+
+    const UClass* ObjectClass = InjectionObject->GetClass();
+    if (TArray<UObject*>* Objects = Instance->RegisteredObjects.Find(ObjectClass))
+    {
+        Objects->Remove(InjectionObject);
+    }
+}
+
+/* ------------------------------------------------------------------------------- */
+
+template<class T>
+T* UDependencyInjector::GetRegisteredObject()
+{
+    if (!Instance)
+    {
+        // The instance doesn't exist, so no objects are registered
+        return nullptr;
+    }
+
+    const UClass* ObjectClass = T::StaticClass();
+    if (TArray<UObject*>* Objects = Instance->RegisteredObjects.Find(ObjectClass))
+    {
+        if (Objects->Num() > 0)
+        {
+            return Cast<T>((*ObjectArray)[0]); // Return the first registered object of the specified class
+        }
+    }
+
+    return nullptr;
+}
+
+/* ------------------------------------------------------------------------------- */
