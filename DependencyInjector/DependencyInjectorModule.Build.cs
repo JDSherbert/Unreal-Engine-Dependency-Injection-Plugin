dce88e230bffb283b46feaa24b4a7c755a8f1dbe
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2024-01-09 01:19:11 +0000

    Update DependencyInjectorModule.Build.cs

--- DependencyInjector/DependencyInjectorModule.Build.cs
+++ DependencyInjector/DependencyInjectorModule.Build.cs
@@ -1,3 +1,5 @@
+// ©2024 JDSherbert. All Rights Reserved.
+
 using UnrealBuildTool; 
 
 public class DependencyInjectorModule : ModuleRules
@@ -7,3 +9,8 @@ public class DependencyInjectorModule : ModuleRules
         PrivateDependencyModuleNames.AddRange(new string[] {"Core", "CoreUObject", "Engine"});
     } 
 }
+
+// Don't forget to add this module to your:
+// - MyProject.uproject module dependency list (In the project Root)
+// AND
+// - MyProject.Build.cs file module dependency list (Should be in Source/MyProject/MyProject.Build.cs)
